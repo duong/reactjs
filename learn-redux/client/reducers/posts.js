ecebd58f955b4bc948a11016d78866d9e8cd4649
Tran Duy Duong <duong@mmj.vn>
2016-06-09 17:49:19 +0700

    first commit

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,28 @@
+// a reducer takes in two things:
+
+
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+
+
+
+function posts(state = [], action) {
+	switch(action.type) {
+		case 'INCREMENT_LIKES':
+	      const i = action.index;
+	      console.log('state', state);
+	      console.log('a', ...state.slice(0,i));
+	      console.log('b', {...state[i], likes: state[i].likes + 1});
+	      console.log('c', ...state.slice(i+1));
+	      return [
+	        ...state.slice(0,i), // before the one we are updating
+	        {...state[i], likes: state[i].likes + 1},
+	        ...state.slice(i + 1), // after the one we are updating
+	      ]
+		default:
+			return state;
+	}
+	return state;
+}
+export default posts;
