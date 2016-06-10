d44a1393966dfb1fd055f7e6d8e2c25f924bd3fe
Tran Duy Duong <duong@mmj.vn>
2016-06-10 13:39:02 +0700

    the second update

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -11,13 +11,13 @@ function posts(state = [], action) {
 	switch(action.type) {
 		case 'INCREMENT_LIKES':
 	      const i = action.index;
-	      console.log('state', state);
+	      console.log('statexxxxxxxxx', state);
 	      console.log('a', ...state.slice(0,i));
 	      console.log('b', {...state[i], likes: state[i].likes + 1});
 	      console.log('c', ...state.slice(i+1));
 	      return [
 	        ...state.slice(0,i), // before the one we are updating
-	        {...state[i], likes: state[i].likes + 1},
+	        {...state[i], likes: state[i].likes + 2},
 	        ...state.slice(i + 1), // after the one we are updating
 	      ]
 		default:
